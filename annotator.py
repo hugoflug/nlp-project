cbc2304179d5860a7056087e796cd50030227ae9
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-02-25 14:34:44 +0100

    refactored into annotate function

--- annotator.py
+++ annotator.py
@@ -1,26 +1,34 @@
 
-dict_file = open("crosswikis-dict-preprocessed")
-matches = {} 
-for line in dict_file:
-    match = line.strip("\n").split("\t")
-    words = match[0]
-    rest = match[1].split(" ")
-    prob = float(rest[0])
-    entity = rest[1]
-    if not words in matches:
-        matches[words] = []
-    matches[words].append((entity, prob))
+def main():
+    dict_file = open("crosswikis-dict-preprocessed")
+    matches = {} 
+    for line in dict_file:
+        match = line.strip("\n").split("\t")
+        words = match[0]
+        rest = match[1].split(" ")
+        prob = float(rest[0])
+        entity = rest[1]
+        if not words in matches:
+            matches[words] = []
+        matches[words].append((entity, prob))
 
-print("enter query: ")
+    print("enter query: ")
 
-query = input().strip()
-query_words = query.split(" ")
-for length in reversed(range(1, len(query_words) + 1)):  
-    for start_index in range(0, len(query_words) - length + 1):
-        potential_match = query_words[start_index:start_index+length] 
-        potential_match = " ".join(potential_match)
+    query = input().strip()
+    query_words = query.split(" ")
+    annotate(matches, query_words, len(query_words) + 1)
 
-        try:
-            match = matches[potential_match]
-            print("{} -- {} ({})".format(potential_match, match[0][0], match[0][1]))
-        except KeyError: pass
+#index_length = len(query_words) + 1
+
+def annotate(matches, words, index_length):
+    for length in reversed(range(1, index_length)):  
+        for start_index in range(0, len(words) - length + 1):
+            potential_match = words[start_index:start_index+length] 
+            potential_match = " ".join(potential_match)
+
+            try:
+                match = matches[potential_match]
+                print("{} -- {} ({})".format(potential_match, match[0][0], match[0][1]))
+            except KeyError: pass
+
+main()
