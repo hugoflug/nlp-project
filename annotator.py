83337f3d0a0a85d1dc9fcc6bbee6b953cda7685f
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-02-25 14:15:13 +0100

    entities no longer overwritten

--- annotator.py
+++ annotator.py
@@ -7,11 +7,19 @@ for line in dict_file:
     rest = match[1].split(" ")
     prob = float(rest[0])
     entity = rest[1]
-    matches[words] = (entity, prob)
+    if not words in matches:
+        matches[words] = []
+    matches[words].append((entity, prob))
+
+print("enter query: ")
 
 query = input().strip()
 query_words = query.split(" ")
 for length in reversed(range(1, len(query_words) + 1)):  
     for start_index in range(0, len(query_words) - length + 1):
         potential_match = query_words[start_index:start_index+length] 
-        print(potential_match)
+        potential_match = " ".join(potential_match)
+        if matches[potential_match]:
+            print("matches:")
+            for m in matches[potential_match]:
+                print("match: ", m[0])
