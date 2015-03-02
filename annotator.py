19689e61c00771075ab0c26025c2ce920a5a497a
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-03-02 11:40:22 +0100

    renamed 'matches' to 'link_probs'

--- annotator.py
+++ annotator.py
@@ -2,38 +2,38 @@
 def main():
     #file not in git repo due to GitHub size restrictions
     dict_file = open("crosswikis-dict-preprocessed-2")
-    matches = {} 
+    link_probs = {} 
     for line in dict_file:
         match = line.strip("\n").split("\t")
         words = match[0]
         rest = match[1].split(" ")
         prob = float(rest[0])
         entity = rest[1]
-        if not words in matches:
-            matches[words] = []
-        matches[words].append((entity, prob))
+        if not words in link_probs:
+            link_probs[words] = []
+        link_probs[words].append((entity, prob))
 
     print("enter query: ")
 
     query = input().strip()
     query_words = query.split(" ")
     annotations = {}
-    annotate(matches, query_words, len(query_words) + 1, annotations)
+    annotate(link_probs, query_words, len(query_words) + 1, annotations)
 
     for key, value in annotations.items():
         print("{} -- {} ({})".format(key, value[0], value[1]))
 
-def annotate(matches, words, index_length, annotations):
+def annotate(link_probs, words, index_length, annotations):
     for length in reversed(range(1, index_length)):  
         for start_index in range(0, len(words) - length + 1):
             potential_match = words[start_index:start_index+length] 
             potential_match = " ".join(potential_match)
 
             try:
-                match = matches[potential_match]
+                match = link_probs[potential_match]
                 annotations[potential_match] = (match[0][0], match[0][1])
-                annotate(matches, words[:start_index], length + 1, annotations)
-                annotate(matches, words[start_index+length:], length + 1, annotations)
+                annotate(link_probs, words[:start_index], length + 1, annotations)
+                annotate(link_probs, words[start_index+length:], length + 1, annotations)
                 return
             except KeyError: pass
 
