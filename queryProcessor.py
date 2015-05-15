187cf7664315fb12ce85c5913bf471dff76ec759
Hugo Sandelius <hugo.sandelius@gmail.com>
2015-05-15 15:34:11 +0200

    updated evaluator

--- queryProcessor.py
+++ queryProcessor.py
@@ -13,45 +13,61 @@ from bing_annotator import BingAnnotator
 from wiki_annotator import WikipediaAnnotator
 from multi_annotator import MultiAnnotator
 from pp_annotator import PriorProbabilityAnnotator
+from tagme_spotter import TagMeSpotter
 
 def main():
 
     # Create evaluator
     evaluator = Evaluator()
-
+    
     print("\nOUR ANNOTATOR (DEV-SET):")
     print("***********************\n")
     annot = annotator.Annotator(open("crosswikis-dict-preprocessed-2", encoding="utf-8"))
     evaluator.evaluate(annot.annotate, "query-data-dev-set.xml")
-    """
+    
     print("\nOUR ANNOTATOR (TRAIN-SET):")
     print("***********************\n")
     annot = annotator.Annotator(open("crosswikis-dict-preprocessed-2", encoding="utf-8"))
     evaluator.evaluate(annot.annotate, "query-data-train-set.xml")
-    """
+    
 
     print("\nNEW ANNOTATOR (DEV-SET):")
+    print("***********************\n")
+    spotter = TagMeSpotter()
+    bing_annotator = BingAnnotator()
+    wiki_annotator = WikipediaAnnotator()
+    sim = TagMeSimilarity()
+    #spotter = GoldSpotter()
+    #pruner = DumbPruner()
+    entity_linker = EntityLinker(spotter=spotter, similarity=sim, annotator=MultiAnnotator(None, wiki_annotator, bing_annotator))
+    evaluator.evaluate(entity_linker.annotate, "query-data-dev-set.xml")
+    sim.save_cache() # save if we have any new similarities to cache on file
+    bing_annotator.save_cache()
+    wiki_annotator.save_cache()
+    spotter.save_cache()
+    """
+    print("\nNEW ANNOTATOR (TRAIN-SET):")
     print("***********************\n")
     bing_annotator = BingAnnotator()
     wiki_annotator = WikipediaAnnotator()
     sim = TagMeSimilarity()
     #spotter = GoldSpotter()
     #pruner = DumbPruner()
-    entity_linker = EntityLinker(similarity=sim, annotator=MultiAnnotator(None, wiki_annotator))
-    evaluator.evaluate(entity_linker.annotate, "query-data-dev-set.xml")
+    entity_linker = EntityLinker(spotter=spotter, similarity=sim, annotator=MultiAnnotator(None, wiki_annotator, bing_annotator))
+    evaluator.evaluate(entity_linker.annotate, "query-data-train-set.xml")
     sim.save_cache() # save if we have any new similarities to cache on file
     bing_annotator.save_cache()
     wiki_annotator.save_cache()
-    
+    spotter.save_cache()
     """
+    
     print("\nTAGME ANNOTATOR (DEV-SET):")
     print("***********************\n")
     evaluator.evaluate(tagme_annotator.annotate, "query-data-dev-set.xml")
-
+    
     print("\nTAGME ANNOTATOR (TRAIN-SET):")
     print("***********************\n")
     evaluator.evaluate(tagme_annotator.annotate, "query-data-train-set.xml")
-    """
 
 class GoldSpotter(object):
 
